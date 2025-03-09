896c7ad03bd003564ae622f5bdd4d2b8ff5072cb
ArvindFullStackDev <arvindfullstackdev@gmail.com>
2025-03-09 15:55:54 +0530

    Jquery Event Created

--- src/app/jquery-event-binding/jquery-event-binding.component.ts
+++ src/app/jquery-event-binding/jquery-event-binding.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-jquery-event-binding',
+  selector: 'AppJqueryEventBinding',
   imports: [],
   templateUrl: './jquery-event-binding.component.html',
   styleUrl: './jquery-event-binding.component.css'
 })
 export class JqueryEventBindingComponent {
+  JQClickEvent : string = "Angular Click Event";
+  EventValue: string = "";
+  ShowName: string = "";
 
+  agClickEventService() {
+    alert("Button has Click");
+  }
+
+  getValue(event: Event) {
+    const inputElement = (event.target as HTMLInputElement).value;   
+    this.EventValue = inputElement;
+  }
+
+  showName() {
+    this.ShowName = this.EventValue;
+    alert(this.ShowName);
+  }
+  sendEventValue(event: Event) {
+    // Get the value from the textbox
+    const inputElement = (event.target as HTMLElement).previousElementSibling as HTMLInputElement;
+    const textboxValue = inputElement.value;
+   
+    console.log('Textbox value:', textboxValue);
+    alert(textboxValue);
+    // Add your logic to handle the value (e.g., send it to an API)
+  }
+
+  sendValue(value: string) {    
+    alert(value);
+    // Add your logic to handle the value (e.g., send it to an API)
+  }
 }
